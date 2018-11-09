4fcbd96f93bb507f3e6b01ffc0e997a0d7dd84ac
abaccam <abaccam@aidodys.com>
2018-11-09 17:52:33 +0100

    end back

--- common/models/devis.js
+++ common/models/devis.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(Devis) {
+module.exports = function(devis) {
 
 };
--- server/boot/root.js
+++ server/boot/root.js
@@ -6,3 +6,25 @@ module.exports = function(server) {
   router.get('/', server.loopback.status());
   server.use(router);
 };
+module.exports = function(app) {
+  
+  var mysqlDS = app.dataSources.MysqlDS;
+  var profile = app.models.profile;
+  var devis = app.models.devis;
+  
+
+  // first autoupdate the `Author` model to avoid foreign key constraint failure
+  mysqlDS.autoupdate('profile', function(err) {
+    if (err) throw err;
+    console.log('\nAutoupdated table `profile`.');
+
+    mysqlDS.autoupdate('devis', function(err) {
+      if (err) throw err;
+      console.log('\nAutoupdated table `devis`.');
+      // at this point the database table `Book` should have one foreign key `authorId` integrated
+    });
+  });
+  // profile.nestRemoting('devis');
+  
+
+};
